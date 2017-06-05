17c6c723fcac7612f7077df2af5584a21ce696bf
bredele <olivier.wietrich@gmail.com>
2017-06-05 16:17:28 -0600

    Generate custom style based on given mixin

--- index.js
+++ index.js
@@ -4,10 +4,14 @@
  *
  */
 
-module.exports = function() {
+module.exports = function(mixins) {
   return (style) => {
     style = style.split('-')
     const value = style.pop()
-    return `${style.join('-')}:${value};`
+    const rule = style.join('-')
+    const cb = mixins && mixins[rule]
+    return cb
+      ? cb(value)
+      : `${rule}:${value};`
   }
 }
--- test/generator.js
+++ test/generator.js
@@ -11,3 +11,13 @@ test('generate style with given rule $RULE-$VALUE', assert => {
   const css = styles()
   assert.equal(css('margin-top-10rem'), 'margin-top:10rem;')
 })
+
+test('generate style based on constructor mixin', assert => {
+  assert.plan(1)
+  const css = styles({
+    'margin-top': (value) => {
+      return `margin-top:${value}rem;`
+    }
+  })
+  assert.equal(css('margin-top-10'), 'margin-top:10rem;')
+})
