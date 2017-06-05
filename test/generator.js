c866b02f43a0b07964c7bb4b65fd01cf9487c0c2
bredele <olivier.wietrich@gmail.com>
2017-06-05 16:08:16 -0600

    Should generate styles with given rule

---
+++ test/generator.js
@@ -0,0 +1,13 @@
+/**
+ * Tests dependencies.
+ */
+
+const test = require('tape')
+const styles = require('..')
+
+
+test('generate style with given rule $RULE-$VALUE', assert => {
+  assert.plan(1)
+  const css = styles()
+  assert.equal(styles('margin-top-10rem'), 'margin-top:10rem;')
+})
